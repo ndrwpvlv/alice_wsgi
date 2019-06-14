9956f8f2e6ac9cf3cbc521e11cb4c2e3b08dfec0
ndrwpvlv <ndrw.pvlv@gmail.com>
2019-06-14 12:11:06 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as fr:
+    required = fr.read().splitlines()
+
+setup(
+    name='alice_wsgi',
+    version='0.0.51',
+    packages=['alice_wsgi', ],
+    url='https://github.com/ndrwpvlv/mimemiko',
+    license='',
+    author='Andrei S. Pavlov',
+    author_email='ndrw.pvlv@gmail.com',
+    description='alice_wsgi - wsgi micro framework with class based views and modular system',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+)
